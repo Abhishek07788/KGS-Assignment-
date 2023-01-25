ca0b8abdc0ac905960256e3d581821af30a730a1
Abhishek Pratap Solanki <abhisheksolanki1020@gmail.com>
2023-01-25 12:38:17 +0530

    Navbar fixed

--- frontend/src/Navbar/Navbar.jsx
+++ frontend/src/Navbar/Navbar.jsx
@@ -1,66 +1,137 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 import style from "./navbar.module.css";
-import {Button, SimpleGrid, useToast} from "@chakra-ui/react";
-import { useDispatch, useSelector } from 'react-redux';
-import { useEffect, useState } from 'react';
+import { Box, Button, SimpleGrid, Text, useToast } from "@chakra-ui/react";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect, useState } from "react";
 import jwt_decode from "jwt-decode";
-import { ClearFunc, logoutFunc } from '../Redux/user/user.action';
+import { ClearFunc, logoutFunc } from "../Redux/user/user.action";
 
 const Navbar = () => {
-  const { loginData } = useSelector( (store) => store.User );
+  const { loginData } = useSelector((store) => store.User);
   const [useData, setUserData] = useState({});
   const dispatch = useDispatch();
   const toast = useToast();
 
-
   // -------------- (Token Decode) ---------------
-  useEffect( () => {
-    if(loginData){
+  useEffect(() => {
+    if (loginData) {
       setUserData(jwt_decode(loginData.token));
     }
-  }, [loginData])
-
+  }, [loginData]);
 
   // --------------- (Log out) -------------------
   const handleLogout = () => {
-    dispatch(logoutFunc())
-     // ------------ Alert---------- 
-     toast({
+    dispatch(logoutFunc());
+    // ------------ Alert----------
+    toast({
       title: "Log out Successfully!",
-      status: 'success',
+      status: "success",
       duration: 2000,
       isClosable: true,
-      position:"top"
-    })
-    setTimeout(()=>{
-    dispatch(ClearFunc())
-    },2)
-    setUserData({})
-  }
+      position: "top",
+    });
+    setTimeout(() => {
+      dispatch(ClearFunc());
+    }, 2);
+    setUserData({});
+  };
 
   return (
-      <div className={style.maindiv} >
-        <Link to="/"><img width="70px" display={["none","none","block","block"]} src="http://khanglobalstudies.com/images/logos/kgs-logo.png" alt="KSG" /></Link>
-        <SimpleGrid className={style.options} columns={[2,2,4,4]} gap={[2,2,5,5]}>
-        <Link to="/"><Button>Course</Button></Link>
-        {useData.role == "admin" ?
-        <Link to="/admin"><Button>Admin</Button></Link> : "" }
+    <div className={style.maindiv}>
+      <Link to="/">
+        <img
+          width="70px"
+          display={["none", "none", "block", "block"]}
+          src="http://khanglobalstudies.com/images/logos/kgs-logo.png"
+          alt="KSG"
+        />
+      </Link>
+      <SimpleGrid
+        className={style.options}
+        columns={[2, 2, 4, 4]}
+        gap={[2, 2, 5, 5]}
+        display={["none", "none", "block", "block"]}
+      >
+        <Link to="/">
+          <Button>Course</Button>
+        </Link>
+        {useData.role == "admin" ? (
+          <Link to="/admin">
+            <Button>Admin</Button>
+          </Link>
+        ) : (
+          ""
+        )}
 
         {/* ---------- (Conditional rendering) ------------*/}
-        {loginData ? <>
-          <Button>Hi: {useData.name}</Button>
-          <Button onClick={handleLogout}>Log out</Button>
-        </> 
-        :
-        <>
-        <Link to="/login"><Button>Log in</Button></Link>
-        <Link to="/signup"><Button>Sign up</Button></Link>
-        </> 
-        }
-        </SimpleGrid>
-      </div>
-  )
-}
+        {loginData ? (
+          <>
+            <Button>Hi: {useData.name}</Button>
+            <Button onClick={handleLogout}>Log out</Button>
+          </>
+        ) : (
+          <>
+            <Link to="/login">
+              <Button>Log in</Button>
+            </Link>
+            <Link to="/signup">
+              <Button>Sign up</Button>
+            </Link>
+          </>
+        )}
+      </SimpleGrid>
 
-export default Navbar
+      <Box gap={2} display={["flex", "flex", "none", "none"]}>
+        <Link to="/">
+          <Text fontWeight={700}>
+            <u>Course</u>
+          </Text>
+        </Link>
+        {useData.role == "admin" ? (
+          <Link to="/admin">
+            <Text fontWeight={700}>
+              <u>Admin</u>
+            </Text>
+          </Link>
+        ) : (
+          ""
+        )}
+
+        {/* ---------- (Conditional rendering) ------------*/}
+        {loginData ? (
+          <>
+            <Text
+              fontWeight={700}
+              onClick={handleLogout}
+              cursor="pointer"
+              bg="#dc3544"
+              borderRadius={20}
+              color="#ffff"
+              pl="1"
+              pr="1"
+            >
+              Log out
+            </Text>
+            <Text fontWeight={700}>
+              Hi: <span style={{ color: "red" }}>{useData.name}</span>
+            </Text>
+          </>
+        ) : (
+          <>
+            <Link to="/login">
+              <Text bg="#dc3544" borderRadius={20} color="#ffff" pl="1" pr="1">
+                Log in
+              </Text>
+            </Link>
+            <Link to="/signup">
+              <Text fontWeight={700}>Sign up</Text>
+            </Link>
+          </>
+        )}
+      </Box>
+    </div>
+  );
+};
+
+export default Navbar;
